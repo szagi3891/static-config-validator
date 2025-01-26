09bdcc2e194dcd827962b66b69638e301c03e8a8
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2025-01-26 07:26:05 +0100

    init

---
+++ main.ts
@@ -0,0 +1,13 @@
+console.info('hello world');
+
+
+//TODO - walidacja deno.json
+
+//TODO - walidacja tsconfig.json
+
+//TODO - walidacja eslintów
+
+// "compilerOptions": {
+//     "noUncheckedIndexedAccess": true,
+//     "noPropertyAccessFromIndexSignature": true
+//   }
